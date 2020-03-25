f73fcff30e6db9191e3812b1dc03a886e2b42714
guigas19 <guilhermecaetano19@gmail.com>
2020-03-25 00:05:42 -0300

    Criado o backend da aplicação

---
+++ backend/src/controllers/OngsController.js
@@ -0,0 +1,29 @@
+const crypt = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+
+    async index(req, res) {
+        const ongs = await connection('ongs').select('*');
+    
+        return res.json(ongs);
+    },
+
+    async create(req, res) {
+
+        const {name, email, whatsaap, city, uf} = req.body;
+    
+        const id = crypt.randomBytes(4).toString('HEX')
+    
+        await connection('ongs').insert({
+            id, 
+            name,
+            email,
+            whatsaap,
+            city, 
+            uf
+        });
+    
+        return res.json({ id });
+    }
+}
